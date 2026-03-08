2ce1d9e47cdce29be7906ab739656e872512152d
julienguyet <jules.guyet@gmail.com>
2026-03-08 20:20:50 +0100

    now connecting to deployed docker mcp

--- scripts/stylist_agent/agent.py
+++ scripts/stylist_agent/agent.py
@@ -3,25 +3,23 @@ from google.adk.models.lite_llm import LiteLlm
 from google.adk.tools.mcp_tool import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
 from google.genai import types
+import os
 
 # ----- DEFINE SYSTEM PROMPT ----- #
-SYSTEM_PROMPT = """You are a Stylist workingt at H&M. You have a strong sense of fashion and your role is to assist customers in their shopping experience. 
-You provide personalize recommendations as well as guidance to optimize their customer experience.
-As of today, you can provide assistance only for custonmers based in Portugal.
+file_path = os.path.join(os.path.dirname(__file__), 'system_prompt.txt')
 
-When looking for stores information:
-- always look for closest store to customer address
-- if the customer prefers to not disclose its address, look for all stores in given city and when listing them, always put at top a flagship store if available."""
+with open(file_path, 'r') as file:
+    SYSTEM_PROMPT = file.read()
 
 # ----- DEFINE AGENT ----- #
 root_agent = LlmAgent(
     model=LiteLlm(model="anthropic/claude-haiku-4-5"),
-    name="Jules",
+    name="Erling",
     instruction=SYSTEM_PROMPT,
     tools=[
         McpToolset(
             connection_params=StreamableHTTPConnectionParams(
-                url='http://127.0.0.1:8000/mcp',
+                url='http://mcp-server:8001/mcp',
                 timeout=120
             ),
         )
